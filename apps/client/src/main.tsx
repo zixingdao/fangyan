5ea82dc9ea740c011e9e758f723577a41960068f
zixingdao <zixingdao@users.noreply.github.com>
2026-02-11 22:12:56 +0800

    chore: trigger deployment with HashRouter fix

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Force rebuild trigger: Switch to HashRouter for CloudBase compatibility
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
