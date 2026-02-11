6574b575adc83bdfdddeda40c2eceea7fc795c5d
zixingdao <zixingdao@users.noreply.github.com>
2026-02-11 20:39:24 +0800

    修复

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -53,7 +53,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.PORT || 80;
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log(`Swagger documentation is available at: ${await app.getUrl()}/docs`);
